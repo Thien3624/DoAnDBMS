690d82ceab6957834eea8b9633bdc47ff5ef07fb
“nhantran1104” <trandainhan.work@gmail.com>
2024-10-19 13:50:36 +0700

    KetnoiDatabase

---
+++ DAL/DBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class DBConnection
+    {
+        string sqlStr = @"Data Source=.;Initial Catalog=QuanLyQuanAn;User ID=sa;Password=***********;Trust Server Certificate=True";
+        SqlConnection sqlCon = null;
+        
+        public DBConnection() { 
+            sqlCon = new SqlConnection(sqlStr);
+        }
+
+        public SqlConnection GetSqlConnection()
+        {
+            return  sqlCon;
+        }
+    }
+}
