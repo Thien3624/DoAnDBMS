ec94c803620389faa6a3d03635304a4d8b443bd2
“nhantran1104” <trandainhan.work@gmail.com>
2024-10-19 14:53:20 +0700

    model

---
+++ BLL/BanAn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    internal class BanAn
+    {
+        private int maBanAn;
+        private string trangThai;
+        private string sucChua;
+
+        public BanAn(int maBanAn, string trangThai, string sucChua)
+        {
+            this.maBanAn = maBanAn;
+            this.trangThai = trangThai;
+            this.sucChua = sucChua;
+        }
+
+        public int MaBanAn { get => maBanAn; set => maBanAn = value; }
+        public string TrangThai { get => trangThai; set => trangThai = value; }
+        public string SucChua { get => sucChua; set => sucChua = value; }
+    }
+}
