7aa5c229aadfcfaecf2c366863a0514159fd34bf
“nhantran1104” <trandainhan.work@gmail.com>
2024-10-23 12:28:59 +0700

    themMonAnVaoDonHang

--- GUI/UC_ThanhPhan/O_MonAn.cs
+++ GUI/UC_ThanhPhan/O_MonAn.cs
@@ -12,15 +12,26 @@ namespace GUI.UC_ThanhPhan
 {
     public partial class O_MonAn : UserControl
     {
+        public event Action<string, string, int, int> DatMonAn;
+        private int giaMonAn;
+
         public O_MonAn()
         {
             InitializeComponent();
         }
-        public void themMonAn(string tenMonAn, int gia, Image images)
+        public void themMonAn(string maMonAn, string tenMonAn, int gia, Image images)
         {
+            lb_maMonAn.Text = maMonAn;
             lb_tenMonAn.Text = tenMonAn;
             lb_giaMonAn.Text = "Giá: " + gia.ToString("N0") + " VNĐ";
             picb_anhMonAn.Image = images;
+
+            giaMonAn = gia;
+        }
+
+        private void btn_datMon_Click(object sender, EventArgs e)
+        {
+            DatMonAn(lb_maMonAn.Text, lb_tenMonAn.Text, (int)numeric_soLuong.Value, giaMonAn);
         }
     }
 }
